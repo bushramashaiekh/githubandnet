077f2146f1aae9e829fb3378606d743ff989d046
AMsys <AMsys@DESKTOP-O34V0M5>
2020-02-08 20:52:06 +0200

    the first

---
+++ src/githubtutori/Githubtutori.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package githubtutori;
+
+/**
+ *
+ * @author AMsys
+ */
+public class Githubtutori {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        int m ,b,n;
+        m=5;
+        b=11 ;
+        n= m+b ;
+        // TODO code application logic here
+    }
+    
+}
